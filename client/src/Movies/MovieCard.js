936d39f781abfb9de0fa592fdda040c5540ad9f2
ph5500 <philfives@gmail.com>
2020-03-23 17:34:49 -0600

    added home button functionality

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom'
+import Movie from './Movie'
+import axios from 'axios'
 
-const MovieCard = props => {
-  return;
+// const MovieCard = props => {
+//   return;
+// };
+class MovieCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movie: this.props.movies
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      // movies: this.props.movies
+
+    })
+    // const id = this.props.match.params.id
+    // this.fetchMovie(id)
+  }
+
+
+
+  fetchMovie = id => {
+    axios
+      .get(`http://localhost:5000/api/movies/`)
+      .then(response => {
+        this.setState({
+          movies: response.data
+        })
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+
+
+  render() {
+    // if (!this.state.movie) {
+    //   return <div>Loading movie information...</div>;
+    // }
+    return (
+      <div>
+        <Link to={`/movies/${this.props.id}`} >
+          <h2>{this.props.title}</h2>
+        </Link>
+        <div className="movie-director">
+          Director: <em>{this.props.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{this.props.metascore}</strong>
+        </div>
+        <h3>Actors:</h3>
+
+      </div>
+    )
+  }
+
 };
-
 export default MovieCard;
